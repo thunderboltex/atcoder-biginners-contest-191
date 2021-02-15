0a84db95b9c4e0410c8ff370e254e5579f68510f
thunderboltex <momi41925-orange@kfy.biglobe.ne.jp>
2021-02-15 16:06:59 +0900

    E.py 108 minutes

---
+++ E.py
@@ -0,0 +1,38 @@
+import heapq
+
+N, M = map(int, input().split())
+
+route = [{} for _ in range(N)]
+
+for i in range(M):
+    A, B, C = map(int, input().split())
+
+    A -= 1
+    B -= 1
+
+    route[A][B] = min(C, route[A].get(B, float('inf')))
+
+for j in range(N):
+    hq = [(0, j)]
+
+    seen = [0] * N
+
+    dist = [float('inf')] * N
+
+    while hq:
+        p, q = heapq.heappop(hq)
+
+        if dist[q] < p:
+            continue
+        if seen[q]:
+            break
+
+        seen[q] = 1
+
+        for k in route[q]:
+            if(k == j or not seen[k]) and p + route[q][k] < dist[k]:
+                dist[k] = p + route[q][k]
+
+                heapq.heappush(hq, (dist[k], k))
+
+print(-1 if dist[i] == float('inf') else dist[i])
